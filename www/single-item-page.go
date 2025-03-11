2ff7a522deaf01361267ebf1e644bcd657e9225d
Naser Mirzaei <nasermirzaei89@gmail.com>
2025-03-11 01:08:19 -0400

    Fix single item issues

--- www/single-item-page.go
+++ www/single-item-page.go
@@ -12,20 +12,45 @@ func (h *Handler) singleItemPageHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		itemID := r.PathValue("itemId")
 
-		item, err := h.listingSvc.GetPublishedItem(r.Context(), itemID)
-		if err != nil {
-			if errors.As(err, &listing.ItemWithIDNotFoundError{}) {
-				h.notFoundPageHandler()(w, r)
+		preview := r.URL.Query().Get("preview") == "true"
+
+		var (
+			item *listing.Item
+			err  error
+		)
+
+		if preview {
+			item, err = h.listingSvc.GetItem(r.Context(), itemID)
+			if err != nil {
+				if errors.As(err, &listing.ItemWithIDNotFoundError{}) {
+					h.notFoundPageHandler()(w, r)
+
+					return
+				}
+
+				err = errors.Wrapf(err, "failed to get item with id '%s'", itemID)
+
+				w.WriteHeader(http.StatusInternalServerError)
+				templ.Handler(templates.HTML(templates.ErrorPage(err), templates.ErrorHead())).ServeHTTP(w, r)
 
 				return
 			}
+		} else {
+			item, err = h.listingSvc.GetPublishedItem(r.Context(), itemID)
+			if err != nil {
+				if errors.As(err, &listing.ItemWithIDNotFoundError{}) {
+					h.notFoundPageHandler()(w, r)
 
-			err = errors.Wrapf(err, "failed to get published item with id '%s'", itemID)
+					return
+				}
 
-			w.WriteHeader(http.StatusInternalServerError)
-			templ.Handler(templates.HTML(templates.ErrorPage(err), templates.ErrorHead())).ServeHTTP(w, r)
+				err = errors.Wrapf(err, "failed to get published item with id '%s'", itemID)
 
-			return
+				w.WriteHeader(http.StatusInternalServerError)
+				templ.Handler(templates.HTML(templates.ErrorPage(err), templates.ErrorHead())).ServeHTTP(w, r)
+
+				return
+			}
 		}
 
 		head := templates.Head{
