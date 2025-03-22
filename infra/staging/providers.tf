941babfcf7bb8dab91163af8371445d95dc1bfe1
Naser Mirzaei <nasermirzaei89@gmail.com>
2025-03-22 19:43:04 -0400

    Add helm provider

--- infra/staging/providers.tf
+++ infra/staging/providers.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.35.1"
+      version = "2.36.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
     }
   }
 }
